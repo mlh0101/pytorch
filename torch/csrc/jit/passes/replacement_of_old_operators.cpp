9ea6318a3689070ee686c66e9bcab7000b9b5927
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2021-12-16 15:10:18 -0800

    Bump version number to 7 and compile old operators with old schema
    ghstack-source-id: 4eb91c9dbdbc04794bb7efb58a669652e909d527
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/68358

--- torch/csrc/jit/passes/replacement_of_old_operators.cpp
+++ torch/csrc/jit/passes/replacement_of_old_operators.cpp
@@ -2,6 +2,7 @@
 
 #include <c10/util/Exception.h>
 #include <caffe2/serialize/versions.h>
+#include <torch/csrc/jit/frontend/schema_matching.h>
 #include <torch/csrc/jit/ir/irparser.h>
 #include <torch/csrc/jit/operator_upgraders/upgraders.h>
 #include <torch/csrc/jit/operator_upgraders/utils.h>
@@ -27,9 +28,7 @@ struct OldOpsReplacer {
     int updated_version = caffe2::serialize::kMinSupportedFileFormatVersion;
     while (node) {
       if (auto schema = node->maybeSchema()) {
-        auto schema_name = schema->name() +
-            (schema->overload_name() != "" ? "." + schema->overload_name()
-                                           : "");
+        auto schema_name = getFullSchemaName(*schema);
         // this implies there was a version bump because of this operator
         auto version_entry = get_operator_version_map().find(schema_name);
         if (version_entry != get_operator_version_map().end()) {
