b8e3347788d7e0d603c61f76f16149e6970c6a23
John Clow <jclow@fb.com>
2021-12-16 15:11:02 -0800

    Building Default Inference for Device Type
    ghstack-source-id: b43ff6197c83f3d78ba54944767c57d0a14cefb7
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69049

---
+++ torch/csrc/jit/passes/device_type_analysis.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Windows Torch API Macro shouldnt' be needed
+#include <torch/csrc/WindowsTorchApiMacro.h>
+#include <torch/csrc/jit/ir/ir.h>
+
+namespace torch {
+namespace jit {
+struct Graph;
+
+// Transposes the weight matrix for frozen linear modules.
+// and converts it into a matmul
+
+// Should support everything except for when the device type is passed in as an arg
+TORCH_API bool DeviceTypePropagation(std::shared_ptr<Graph> graph);
+
+} // namespace jit
+} // namespace torch
